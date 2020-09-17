16ead603ae6c79920905946fdc7c34560bcc2a85
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-09-17 05:22:58 +0000

    formatting

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import Particle from './Components/Particle';
 
 function App() {
   return (
-    <div className="App" style={{position: 'relative'}}>
-      <div style={{position: 'absolute', width: "100%"}}>
+    <div className="App" style={{ position: 'relative' }}>
+      <div style={{ position: 'absolute', width: "100%" }}>
         <Particle />
       </div>
       <div>
         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code>!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
+        </header>
       </div>
     </div>
   );
